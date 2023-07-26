60b7ee6876f3469c180426266502e4e9a3699e91
Theo Kanin <h.dubium@gmail.com>
2023-07-26 13:04:16 +0300

    jar: Run jar on macos from release using sh script

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,10 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use directories::ProjectDirs;
+use log::info;
+use std::path::PathBuf;
+use tauri::api::process::Command;
 use tauri_plugin_log::LogTarget;
 
 fn main() {
@@ -10,60 +14,27 @@ fn main() {
                 .targets([LogTarget::Webview, LogTarget::Stdout, LogTarget::LogDir])
                 .build(),
         )
+        .invoke_handler(tauri::generate_handler![run_jar])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
 #[tauri::command]
-fn run_jar(handle: tauri::AppHandle) {
-    println!("Running jar");
-    let resource_path = handle
-        .path_resolver()
-        .resolve_resource("assets/sample.jar")
-        .expect("failed to resolve resource");
+fn run_jar() {
+    let jar_path = get_jar_path();
 
-    let java_exe = match get_java_executable() {
-        Some(java) => java,
-        None => {
-            println!("Failed to find Java executable.");
-            return;
-        }
-    };
-
-    let mut child = std::process::Command::new(java_exe)
-        .arg("-jar")
-        .arg(resource_path)
+    let _ = Command::new_sidecar("launch_jar")
+        .expect("failed to create `my-sidecar` binary command")
+        .args(&[jar_path.to_str().unwrap()])
         .spawn()
-        .expect("failed to spawn child process");
+        .expect("Failed to spawn sidecar");
 
-    let exit_status = child.wait().expect("failed to wait on child process");
-    println!("child exited with: {}", exit_status);
+    info!("Running sidecar from rust");
 }
 
-fn get_java_executable() -> Option<String> {
-    let java_home = std::env::var("JAVA_HOME").ok()?;
-    let java_exe = format!("{}/bin/java", java_home);
-
-    if std::path::Path::new(&java_exe).exists() {
-        return Some(java_exe);
-    }
-
-    #[cfg(target_os = "windows")]
-    let command = "where";
-    #[cfg(not(target_os = "windows"))]
-    let command = "which";
-
-    let output = std::process::Command::new(command)
-        .arg("java")
-        .output()
-        .ok()?;
-
-    let path = String::from_utf8(output.stdout).ok()?;
-    let path = path.trim();
-
-    if path.is_empty() {
-        None
-    } else {
-        Some(path.to_string())
-    }
+fn get_jar_path() -> PathBuf {
+    let project_dirs = ProjectDirs::from("com", "cubich", "cubich_launcher").unwrap();
+    let data_dir = project_dirs.data_dir();
+    let jar_path = data_dir.join("sample.jar");
+    jar_path
 }

a92a24ea9406daef44aa8d3c55b4ad9558c27431
Theo Kanin <h.dubium@gmail.com>
2023-09-21 23:58:25 +0200

    fix: Add log level filter for less polluted logs

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use log::LevelFilter;
 use tauri_plugin_log::LogTarget;
 
 fn main() {
     tauri::Builder::default()
+        // .invoke_handler(tauri::generate_handler![downloadJava])
         .plugin(
             tauri_plugin_log::Builder::default()
                 .targets([LogTarget::Webview, LogTarget::Stdout, LogTarget::LogDir])
+                .level(LevelFilter::Info)
                 .build(),
         )
         .run(tauri::generate_context!())
